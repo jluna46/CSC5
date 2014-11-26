6f01b168ca423ac62543b7b3bc48d0187486ac92
jluna46 <jluna46@student.rcc.edu>
2014-11-26 10:49:22 -0800

    Signed-off-by: jluna46 <jluna46@student.rcc.edu>

---
+++ Lab12/lab12.2/main.cpp
@@ -0,0 +1,23 @@
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+const int NAMESIZE=15;
+const int MAXRECORDS=50;
+struct Grades{
+    char name[NAMESIZE +1];
+    int test1;
+    int test2;
+    int final;
+};
+
+typedef Grades gradeType[MAXRECORDS];
+
+
+int main(int argc, char** argv) {
+    
+
+    return 0;
+}
+
